5aa79e623e9fdaae93b0c73bfbfcea0caac94f07
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-04-12 00:31:13 +0600

    job data added

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+//React router
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
