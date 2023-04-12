7f3458dd7d3e68d13215eecf327ff2a2d54eab65
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-04-12 15:29:33 +0600

    job details and statistics added

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import './JobDetails.css'
+
+import salaryIcon from '../../assets/Icons/salary.png'
+import jobIcon from '../../assets/Icons/jobtitle.png'
+import phoneIcon from '../../assets/Icons/phone.png'
+import emailIcon from '../../assets/Icons/email.png'
+import locationIcon from '../../assets/Icons/location.png'
 const JobDetails = () => {
     const { id } = useParams();
     const [details, setDetails] = useState({});
@@ -12,27 +18,45 @@ const JobDetails = () => {
     }, [allDetails])
     // console.log(details)
     // console.log(details.desescription);
-    const { desescription, responsibility, requirements, experience, salary, title, phone, email } = details;
+    const { desescription, responsibility, requirements, experience, salary, title, phone, email , address} = details;
     return (
         <div className='mt-4 mx-56'>
             <h2 className='text-5xl text-center font-bold my-20'>Job details</h2>
             {/* <div className='job-details-container'> */}
             <div className='flex gap-10'>
                 <div>
-                    <p><span className='font-bold'>Job Description:</span> {details.desescription}</p>
-                    <p><span>Job Responsibility:</span> {responsibility}</p>
-                    <p><span>Educational Requirements:</span>{requirements}</p>
-                    <p><span>Experiences: </span>{experience}</p>
+                    <p className='mb-6'><span className='font-bold'>Job Description:</span> {details.desescription}</p>
+                    <p className='mb-6'><span className='font-bold'>Job Responsibility:</span> {responsibility}</p>
+                    <p className='mb-6'><span className='font-bold mb-4'>Educational Requirements: <br /></span>{requirements}</p>
+                    <p className='mb-6'><span className='font-bold mb-4'>Experiences: <br /></span>{experience}</p>
                 </div>
                 <div >
-                    <div className='bg-purple-100'>
-                        <h2>Job Details</h2>
+                    <div className='job-details-section'>
+                        <h2 className='text-xl font-bold mb-3'>Job Details</h2>
                         <hr />
-                        <p>Salary: {salary}</p>
-                        <p>Job Title: {title}</p>
-                        <h2>Contact information</h2>
-                        <p>Phone:{phone}</p>
-                        <p>Email:{email}</p>
+                        <p className='flex gap-2'>
+                            <img src={salaryIcon} alt="" />
+                            <span className='font-semibold'>Salary:</span> {salary}
+                        </p>
+                        <p className='flex gap-2 my-2'>
+                            <img src={jobIcon} alt="" />
+                            <span className='font-semibold'>Job Title:</span> {title}
+                        </p>
+                        <h2 className='text-xl font-bold my-5 '>
+                            Contact information
+                        </h2>
+                        <p className='flex gap-2'>
+                            <img src={phoneIcon} alt="" />
+                            <span className='font-semibold'>Phone:</span>{phone}
+                        </p>
+                        <p className='flex gap-2 my-2'>
+                            <img src={emailIcon} alt="" />
+                            <span className='font-semibold'>Email:</span>{email}
+                        </p>
+                        <p className='flex gap-2 my-2'>
+                            <img src={locationIcon} alt="" />
+                            <span className='font-semibold'>Address:</span>{address}
+                        </p>
                     </div>
                     <div>
                         <button className='btn-start-applying mt-5 w-full text-white'>Apply Now</button>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+
+const data = [
+    {
+        name: 'Assignment 01', mark: 60
+    },
+    {
+        name: 'Assignment 02', mark: 60
+    },
+    {
+        name: 'Assignment 03', mark: 60
+    },
+    {
+        name: 'Assignment 04', mark: 60
+    },
+    {
+        name: 'Assignment 05', mark: 59
+    },
+    {
+        name: 'Assignment 06', mark: 60
+    },
+    {
+        name: 'Assignment 07', mark: 60
+    },
+    {
+        name: 'Assignment 08', mark: 60
+    },
+]
 
 const Statistics = () => {
     return (
-        <div>
-            <h2>Statistics</h2>
+        <div className='mt-20'>
+            <LineChart width={1000} height={500} data={data} margin={{ top: 5, right: 50, bottom: 5, left: 0 }} className='mx-auto'>
+            <Line type="monotone" dataKey="mark" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="8 8" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+        </LineChart>
         </div>
     );
 };
