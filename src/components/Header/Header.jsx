e0a1d50aea4bc5a27bc17266ceb1996b9186d760
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-04-12 22:03:59 +0600

    Rechart Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,13 +8,13 @@ const Header = () => {
                 <h1 className='text-3xl font-bold'>Career Starter</h1>
             </div>
             <div className='font-semibold grid grid-cols-2 md:grid-cols-4'>
-                <Link to='/' className='mx-5 hover:text-[#7E90FE]'>Home</Link>
-                <Link to='/statistics' className='mx-5 hover:text-[#7E90FE]'>Statistics</Link>
-                <Link to='appliedjobs' className='mx-5 hover:text-[#7E90FE]'>Applied Jobs</Link>
-                <Link to='/blog' className='mx-5 hover:text-[#7E90FE]'>Blog</Link>
+                <Link to='/' className='mx-5 my-2 md:my-0 hover:text-[#7E90FE]'>Home</Link>
+                <Link to='/statistics' className='mx-5 my-2 md:my-0 hover:text-[#7E90FE]'>Statistics</Link>
+                <Link to='appliedjobs' className='mx-5 my-2 md:my-0 hover:text-[#7E90FE]'>Applied Jobs</Link>
+                <Link to='/blog' className='mx-5 my-2 md:my-0 hover:text-[#7E90FE]'>Blog</Link>
             </div>
             <div>
-                <button className='btn-start-applying text-white'>Start Applying</button>
+                <button className='btn-start-applying mt-5 md:my-0 text-white'>Start Applying</button>
             </div>
         </nav>
     );
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,43 +1,66 @@
 import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
-
-const data = [
-    {
-        name: 'Assignment 01', mark: 60
-    },
-    {
-        name: 'Assignment 02', mark: 60
-    },
-    {
-        name: 'Assignment 03', mark: 60
-    },
-    {
-        name: 'Assignment 04', mark: 60
-    },
-    {
-        name: 'Assignment 05', mark: 59
-    },
-    {
-        name: 'Assignment 06', mark: 60
-    },
-    {
-        name: 'Assignment 07', mark: 60
-    },
-    {
-        name: 'Assignment 08', mark: 60
-    },
-]
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+
+    const marks = [
+        {
+            id: 1,
+            assignment: "Assignment-01",
+            mark: 60
+        },
+        {
+            id: 2,
+            assignment: "Assignment-02",
+            mark: 60
+        },
+        {
+            id: 3,
+            assignment: "Assignment-03",
+            mark: 60
+        },
+        {
+            id: 4,
+            assignment: "Assignment-04",
+            mark: 60
+        },
+        {
+            id: 5,
+            assignment: "Assignment-05",
+            mark: 59
+        },
+        {
+            id: 6,
+            assignment: "Assignment-06",
+            mark: 60
+        },
+        {
+            id: 7,
+            assignment: "Assignment-07",
+            mark: 60
+        },
+        {
+            id: 8,
+            assignment: "assignment 8",
+            mark: 60
+        }
+    ];
+
     return (
-        <div className='mt-20'>
-            <LineChart width={1000} height={500} data={data} margin={{ top: 5, right: 50, bottom: 5, left: 0 }} className='mx-auto'>
-            <Line type="monotone" dataKey="mark" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" strokeDasharray="8 8" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-        </LineChart>
+        <div className='md:my-10'>
+            <h2 className='mb-10 font-bold text-center text-2xl'>Marks Chart</h2>
+            <AreaChart className='mx-auto'
+                width={1200}
+                height={500}
+                data={marks}>
+                
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="assignment"></XAxis>
+                <YAxis dataKey="mark"></YAxis>
+                <Tooltip></Tooltip>
+                <Area type="monotone" dataKey="mark" stroke="#d584d883" fill="#8884d8" />
+
+            </AreaChart>
         </div>
     );
 };
